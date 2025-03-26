eb152416b9bf289b31b4f11127ce5b3ff6c4c222
anmol843 <anmolsharma97565@gmail.com>
2025-03-26 22:36:24 +0530

    update mongo

--- server.js
+++ server.js
@@ -15,8 +15,14 @@ app.use(express.json());
 app.use(cors()); // Enable CORS
 app.use("/uploads", express.static(path.join(__dirname, "uploads"))); // Serve uploaded images
 
-// MongoDB Connection
-mongoose.connect("process.env.MONGO_URI");
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log("MongoDB Connected"))
+.catch(err => console.error("MongoDB Connection Error:", err));
+
 
 
 // Models
